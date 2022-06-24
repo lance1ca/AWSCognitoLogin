e6d7f52f995aba311d839449335467d894e66c4c
Lance Rogan <77311869+lance1ca@users.noreply.github.com>
2022-06-24 11:36:07 -0700

    demo working of promise

--- AWSCognito/cognito.js
+++ AWSCognito/cognito.js
@@ -24,9 +24,9 @@ const poolData = {
 //------------------------------------------------------------------------------------------------------------------------------
     //Register user function, this function registers a user into the user pool.
     //This function takes in parameters for each input entered on the register page
-    function RegisterUser(name,gender,email,phone,password){
-
-        return new Promise((resolve, reject)=>{
+    async function RegisterUser(name,gender,email,phone,password){
+       
+        // return new Promise((resolve, reject)=>{
         //initialize attributelist
         var attributeList = [];
         //here we push each new cognito user attribute into the attribute list with the format ___ : ____ where it is name: ___ value: ___
@@ -35,6 +35,7 @@ const poolData = {
         attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"email",Value:email}));
         attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"phone_number",Value:phone}));
 
+        let promise = new Promise((resolve, reject) => {
         //here we sign the user up into the pull and we pass in the email (username), password, and the attribute list initialized above
         userPool.signUp(email, password, attributeList, null, function(err, result){
             //if there is an error, we send the error message to the console and back to the page for the user to see
@@ -42,6 +43,7 @@ const poolData = {
                 //console.log(err);
                 console.log("ERROR MESSAGE:"+err.message)
                 console.log('here first3')
+                let returnedMessage = err.message
                 reject(err.message)
             }else{
 
@@ -53,12 +55,20 @@ const poolData = {
             console.log('User name is ' + cognitoUser.getUsername());
             console.log('User object is:\n'+cognitoUser.toString())
             console.log('here first2')
+            let returnedMessage = "YES"
             resolve("Account created successfully! A verification code was sent to your email, please verify below.");
             }
         });
-        console.log('here first1')
-        reject("WHY DO U GO HERE FIRST :((((((((((((((((((((((")
-    })
+
+
+        
+            
+        });
+        let result = await promise;
+        console.log(result)
+        
+        // reject("WHY DO U GO HERE FIRST :((((((((((((((((((((((")
+    // })
     }
 
 
